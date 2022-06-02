2983548048523280800ed4ae62d3507614d3edff
aybarskocoglu <aybarskocoglu@localhost>
2022-06-02 13:08:35 -0400

    better strategy (than ian)

--- src/BrownJacksonKocogluSiddiquiGreedStrategy.java
+++ src/BrownJacksonKocogluSiddiquiGreedStrategy.java
@@ -8,16 +8,30 @@ public class BrownJacksonKocogluSiddiquiGreedStrategy extends GreedStrategy {
 	}
 	
 	public int choose(GreedOption[] options, int[] dice, int bank) {
+		/*
 		for (int i = 1; i < options.length; i++) {
 			if (options[i].optionType() == GreedOption.SCORE) {
 				if (((ScoringCombination) options[i]).getValue() != 50) {
-					return GreedOption.ROLLAGAIN;
+					return 1; //bank scoring combination
 				}
 			}
 		}
 		if (dice.length <= 3) {
-			return GreedOption.SCORE;
+			return 2; //end turn
 		}
-		return GreedOption.ENDTURN;
+		
+		return 0; //end turn
+		*/
+		for (int i = 0; i < options.length; i++) {
+			if (options[i].optionType() == GreedOption.SCORE) {
+				if (((ScoringCombination) options[i]).getValue() != 50 || dice.length <= 3) {
+					return 0; //bank scoring combination
+				}
+			}
+		}
+		if (dice.length <= 2) {
+			return 1; //end turn
+		}
+		return 0; //roll again
 	}
 }
--- src/GreedGame.java
+++ src/GreedGame.java
@@ -328,11 +328,11 @@ public class GreedGame {
 	}
 	
 	public static void main(String[] args){
-		int score = 0;
-		for (int i = 0; i < 10000; i++) {
+		double score = 0;
+		for (int i = 0; i < 100000; i++) {
 			score += new GreedGame(false).evaluatePlayer(new ComputerGreedStrategyPlayer(new BrownJacksonKocogluSiddiquiGreedStrategy()),1);
 		}
-		score /= 10000;
+		score /= 100000.0;
 		System.out.println(score);
 	}
 }
