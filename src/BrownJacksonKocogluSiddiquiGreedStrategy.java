1b40b8286975c1ef7675489e3e3d50c80b4e1d4e
aybarskocoglu <aybarskocoglu@localhost>
2022-06-10 12:32:23 -0400

    final strategy

--- src/BrownJacksonKocogluSiddiquiGreedStrategy.java
+++ src/BrownJacksonKocogluSiddiquiGreedStrategy.java
@@ -7,37 +7,13 @@ public class BrownJacksonKocogluSiddiquiGreedStrategy extends GreedStrategy {
 		return "Brown, Jackson, Kocoglu, Siddiqui";
 	}
 
-	public int choose(GreedOption[] options, int[] dice, int bank) {
-		int bonusTurns = 0;
-		//gets about 361 points
-		/*
-		for (int i = 0; i < options.length; i++) {
-			if (options[i].optionType() == GreedOption.SCORE) {
-				if (((ScoringCombination) options[i]).getValue() != 50 || dice.length <= 3) {
-					return 0; //bank scoring combination
-				}
-			}
-		}
-		if (dice.length <= 2) {
-			return 1; //end turn
-		}
-		return 0; //roll again
-		*/
-		//gets about 404 points
-//		if(bank >= 4000) {
-//			return options.length - 1; //end turn
-//		}
-		
-		if(dice.length == 0) {
-				bonusTurns++;
-				return options.length - 2; //roll again
-		}
+	public int choose(GreedOption[] options, int[] dice, int bank) {		
 		if (options[0].optionType() == GreedOption.SCORE) {
 			if (((ScoringCombination) options[0]).getValue() > 50 || dice.length <= 3) { //if highest scoring option more than 50
 				return 0; //bank scoring combination
 			}
 		}
-		if (dice.length <= 3) {
+		if (dice.length <= 3 && dice.length != 0) {
 			return options.length - 1; //end turn
 		}
 		return options.length - 2; //roll again
